9b30a8eeb8ff4a9bcb9a2df9b1bbebc0ca9c5563
=sachinsingh33 <=ds76619@gmail.com>
2016-09-24 18:24:55 +0530

    first commit

---
+++ sachinnew/app/Http/routes.php
@@ -0,0 +1,98 @@
+<?php
+
+Route::get('/','PageController@index');
+
+Route::get('/index','PageController@home');
+
+Route::get('/services','PageController@services');
+
+Route::get('/portfolio','PageController@portfolio');
+
+Route::get('/shortcodes','PageController@shortcodes');
+
+Route::get('/mail','PageController@mail');
+
+Route::get('/single','PageController@single');
+
+Route::get('/Home1','PageController@Home1');
+
+Route::get('/Mobilephones','PageController@Mobilephones');
+
+Route::get('/Tablets','PageController@Tablets');
+
+Route::get('/Laptops','PageController@Laptops');
+
+Route::get('/Motherboards','PageController@Motherboards');
+
+Route::get('/Processors','PageController@Processors');
+
+Route::get('/Offers','PageController@Offers');
+
+Route::get('/SignUp','PageController@SignUp');
+
+Route::get('/Login','PageController@Login');
+
+
+
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/custom', function () {
+    return view('about');
+});
+
+Route::get('/andugundu', function () {
+    return view('about');
+});
+Route::get('/dhishum', function () {
+    return view('hello');
+});
+Route::get('/shop', function () {
+    return view('Shopchus');
+});*/
+
+
+
+
+/*Route::get('/index', function () {
+    return view('pages.index');
+});
+Route::get('/services', function () {
+    return view('pages.services');
+});
+Route::get('/portfolio', function () {
+    return view('pages.portfolio');
+});
+Route::get('/short-codes', function () {
+    return view('pages.short-codes');
+});
+Route::get('/mail', function () {
+    return view('pages.mail');
+});
+Route::get('/single', function () {
+    return view('pages.single');
+});*/
+
+
+
+
+
+
+
